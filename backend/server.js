2e034c3c821b87681b6a4f62daa9cee80839be48
StellaKis <sk53957@fer.hr>
2025-11-09 12:09:06 +0100

    attack za CSRF

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,9 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    httpOnly: true,
+    httpOnly: false,
     secure: true,
-    sameSite: 'lax',
+    sameSite: 'none',
     maxAge: 24*60*60*1000 // 1 dan
   }
 }));
